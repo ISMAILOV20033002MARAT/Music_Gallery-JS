47e76be241d3f2ca69ea684ee21a42aa65775f7b
marat <mrxpro2003@gmail.com>
2022-06-30 16:45:13 +0500

    музика дабавила

--- audeo_2/Music Player in JavaScript/js/music-list.js
+++ audeo_2/Music Player in JavaScript/js/music-list.js
@@ -46,6 +46,13 @@ let allMusic = [
     img: "music-6",
     src: "music-6"
   },
+
+  {
+    name: "Lost Sky - Vision NCS",
+    artist: "NCS Release",
+    img: "music-4",
+    src: "music-7"
+  },
   // like this paste it and remember to give comma after ending of this bracket }
   // {
   //   name: "Here is the music name",
